f8ae16892a2f1630f31eb62854cf2dbf410150bf
AlexBennet88 <ger19989898@gmail.com>
2025-07-18 18:38:39 +0300

    I'm so exausted, I finished Automation Trainer

--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,10 @@ import numpy as np
 import pandas as pd
 import dill
 from src.exception import CustomException
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score
+
+
 
 def save_object(file_path, obj):
     try:
@@ -15,3 +19,26 @@ def save_object(file_path, obj):
 
     except Exception as e:
         raise CustomException(e, sys)
+    
+
+def evaluate_model(X_train, y_train, X_test, y_test, models):
+     try: 
+        report = {}
+
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+
+            model.fit(X_train, y_train)
+
+            y_pred = model.predict(X_test)
+
+            train_model_score = r2_score(y_test, y_pred)
+
+            report[list(models.keys())[i]] = train_model_score
+
+        return report
+     
+
+     except Exception as e:
+        raise CustomException(e, sys)   
+    
